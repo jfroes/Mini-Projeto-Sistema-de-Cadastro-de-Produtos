33c82b16e415e4a759007a486afd5fb2767b03a2
José Froes <josepaulo.froes@gmail.com>
2025-11-15 09:56:34 -0300

    Add entidade Produto com atributos e ID autoincrementado

---
+++ src/entities/Produto.java
@@ -0,0 +1,57 @@
+package entities;
+
+public class Produto {
+    private static Integer GENERATED_ID = 1;
+    private Integer id = 0;
+    private String nome;
+    private Double preco;
+    private Integer quantidade;
+
+    public Produto(){
+    }
+
+    public Produto(String nome, Double preco, Integer quantidade) {
+        this.id = GENERATED_ID++;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", quantidade=" + quantidade +
+                '}';
+    }
+}
